9864b479b2661499e1fecf4c6ba4cd16cf23e5f1
Carlos Moreno <carlos.moreno.villodre@gmail.com>
2017-06-12 15:27:25 +0200

    Add first query to complements table

--- routes/complements.js
+++ routes/complements.js
@@ -1,3 +1,20 @@
 module.exports = (app, connection, cors, corsOptions) => {
+  // GET - Get all complements
+  const getComplements = (req, res) => {
+    connection.query('SELECT * FROM complements', (err, complements) => {
+      if (!err) {
+        if (!complements.length) {
+          res.statusCode = 404
+          res.send({ message: 'Not complements yet' })
+          return
+        }
+        res.send(complements)
+      } else {
+        res.statusCode = 500
+        res.send({ message: 'Opss, something went wrong!' })
+      }
+    })
+  }
 
+  app.get('/complements', cors(corsOptions), getComplements)
 }
