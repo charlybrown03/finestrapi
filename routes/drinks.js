b00b78f639f978f6ad1d7940d2eb3eb67cb12241
Carlos Moreno <carlos.moreno.villodre@gmail.com>
2017-06-12 15:46:13 +0200

    Standarize messages

--- routes/drinks.js
+++ routes/drinks.js
@@ -1,3 +1,5 @@
+const ERROR_MESSAGE = 'Opss, something went wrong!'
+
 module.exports = (app, connection, cors, corsOptions) => {
   // POST - Create new drink
   const addDrink = (req, res) => {
@@ -18,29 +20,38 @@ module.exports = (app, connection, cors, corsOptions) => {
 
   // GET - Get all drinks
   const getDrinks = (req, res) => {
-    connection.query('SELECT * FROM drinks', (err, drink) => {
+    connection.query('SELECT * FROM drinks', (err, drinks) => {
       if (!err) {
-        res.send(drink)
+        if (!drinks.length) {
+          res.statusCode = 404
+          res.send({ message: 'Not drinks yet' })
+          return
+        }
+        res.send(drinks)
       } else {
-        res.statusCode = 404
-        res.send({ message: 'Not drinks yet' })
+        res.statusCode = 500
+        res.send({ message: ERROR_MESSAGE })
       }
     })
   }
 
-  // GET - Return a Drink with specified ID
+  // GET - Return a drink with specified ID
   const findById = (req, res) => {
-    connection.query('SELECT * FROM drinks WHERE id = ?', req.params.id, (err, drink) => {
-      if (!err && drink.length) {
+    connection.query('SELECT * FROM drinks WHERE code = ?', req.params.code, (err, drink) => {
+      if (!err) {
+        if (!drink.length) {
+          res.statusCode = 404
+          res.send({ message: 'This drink does not exist' })
+        }
         res.send(drink)
       } else {
-        res.statusCode = 404
-        res.send({ message: 'This drink don\'t exist' })
+        res.statusCode = 500
+        res.send({ message: ERROR_MESSAGE })
       }
     })
   }
 
   app.post('/drink', cors(corsOptions), addDrink)
-  app.get('/drink/:id', cors(corsOptions), findById)
+  app.get('/drink/:code', cors(corsOptions), findById)
   app.get('/drinks', cors(corsOptions), getDrinks)
 }
