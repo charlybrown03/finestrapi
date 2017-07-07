423386df55b35dbd0e5d0a5f614fbbfa6d629fd3
Carlos Moreno <carlos.moreno.villodre@gmail.com>
2017-07-07 21:15:02 +0200

    Add comments: POST, GET and GET ONE

---
+++ routes/comments.js
@@ -0,0 +1,57 @@
+const ADD_COMMENT = 'INSERT INTO comments (comment, author) values (?, ?)'
+const GET_ALL = 'SELECT * FROM comments'
+
+const FIND_BY_ID = `SELECT * FROM comments WHERE id = ?`
+
+module.exports = (app, connection, cors, corsOptions) => {
+  // POST - Add new comment
+  const add = (req, res) => {
+    const queryParams = [
+      req.body.comment,
+      req.body.author
+    ]
+
+    connection.query(ADD_COMMENT, queryParams, (err, comment) => {
+      if (!err) {
+        const req = {
+          params: {
+            id: comment.insertId
+          }
+        }
+
+        return findById(req, res)
+      }
+      _sendError(res)
+    })
+  }
+
+  // GET - Get all comments
+  const get = (req, res) => {
+    console.info('GET ALL GUESTS')
+    connection.query(GET_ALL, (err, comments) => {
+      if (!err) return res.send(comments)
+
+      _sendError(res)
+    })
+  }
+
+  // GET - Find one by id
+  const findById = (req, res) => {
+    connection.query(FIND_BY_ID, req.params.id, (err, comments) => {
+      console.info('GET ONE COMMENT')
+      if (!err) return res.send(comments[0] || {})
+
+      _sendError(res)
+    })
+  }
+
+  const _sendError = (res) => {
+    res.statusCode = 500
+    res.send({ message: 'Opss, something went wrong!' })
+  }
+
+  app.get('/comments', cors(corsOptions), get)
+  app.options('/comment', cors(corsOptions))
+  app.post('/comment', cors(corsOptions), add)
+  app.get('/comment', cors(corsOptions), findById)
+}
--- routes/router.js
+++ routes/router.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
+  require('./comments')(app, connection, cors, corsOptions)
   require('./complements')(app, connection, cors, corsOptions)
   require('./drinks')(app, connection, cors, corsOptions)
   require('./guests')(app, connection, cors, corsOptions)
